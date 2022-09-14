df42922d8fbb72d26f5155c8f06e50a698d890e6
Kevin Csiffary <kevin.csiffary@gmail.com>
2022-09-14 22:03:39 +0000

    Created hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,10 @@
+# Created by Kevin
+# Sept 14, 2022
+# This program prints "Hello, World!" and has comments
+
+def main():
+    print("Hello, World!")
+    print("This one has comments")
+
+if __name__ == "__main__":
+    main()
